555610594022f25a528265ef90bf2a12918d20f4
aokuyama <ao980s@gmail.com>
2022-02-27 08:00:24 +0900

    add event parser

---
+++ sqs_idempotency/message.py
@@ -0,0 +1,29 @@
+import json
+import unittest
+
+
+class Message:
+    def __init__(self, record=None) -> None:
+        self.record = record
+
+    def body_raw(self):
+        return self.record['body']
+
+    def data(self):
+        return json.loads(self.body_raw())
+
+
+class TestMessage(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        self.message = Message()
+
+    def test_body内jsonを展開(self):
+        self.message.record = {'body': '{"name":123}'}
+        self.assertEqual('{"name":123}', self.message.body_raw())
+        self.assertEqual({"name": 123}, self.message.data())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ sqs_idempotency/messages.py
@@ -0,0 +1,68 @@
+
+from logging import raiseExceptions
+import unittest
+from message import Message
+
+
+class Messages:
+    @classmethod
+    def by_event(cls, event, max=10):
+        msgs = Messages()
+        msgs.parse_event(event, max)
+        return msgs
+
+    def __init__(self, msgs=None) -> None:
+        self._msgs = msgs
+
+    def get(self):
+        return self._msgs
+
+    def _check_error(self, max):
+        msgs = self.get()
+        if msgs is None:
+            return
+        if len(msgs) > max:
+            raise RuntimeError
+
+    def parse_event(self, event, max=10):
+        self._msgs = Messages._parse_event(event)
+        self._check_error(max)
+
+    @classmethod
+    def _parse_event(cls, event):
+        if type(event) is not dict:
+            raise AttributeError()
+        messages = []
+        if 'Records' not in event:
+            return None
+        for record in event['Records']:
+            messages.append(Message(record))
+        return messages
+
+
+class TestMessages(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        self.messages = Messages()
+
+    def test_初期化エラー(self):
+        with self.assertRaises(AttributeError):
+            self.messages.parse_event(None)
+
+    def test_レコードがない(self):
+        self.messages.parse_event({})
+        self.assertIsNone(self.messages.get())
+
+    def test_メッセージ取得(self):
+        self.messages.parse_event({'Records': [{}, {}]}, 3)
+        msgs = self.messages.get()
+        self.assertEqual(2, len(msgs))
+
+    def test_レコードが想定より多い場合エラー(self):
+        with self.assertRaises(RuntimeError):
+            self.messages.parse_event({'Records': [{}, {}, {}]}, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
